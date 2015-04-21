e2ed85f716158f7e1b5943d38ca623f48c86aae5
jtorresgabriel <jorge.torres.degabriel@alumnos.upm.es>
2015-04-21 13:09:58 +0200

    Random1
    Primer fichero random, prueba de uso de githubapp

---
+++ random.js
@@ -0,0 +1,9 @@
+// Math.random  variable aletorio entre 0 y 1
+var numero = MAth.rando();
+
+if (numero <= 0.5){
+  console.log('\n' + numero + 'MENOR que 0,5 \n');
+}
+else {
+  console.log('\n' + numero + 'MAYOR que 0,5 \n');
+}
